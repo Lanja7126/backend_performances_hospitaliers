ff830ca66a47457c4955c2fa20bf9540498aa6e2
RASOLOFOMANANA Aina Herilanja <ainaherilanja14@gmail.com>
2026-09-05 10:32:26 +0300

    debug:server

--- server.js
+++ server.js
@@ -3,18 +3,27 @@ import { connectDB } from "./src/config/db.js";
 import { env } from "./src/config/env.js";
 
 async function demarrer() {
-  await connectDB();
+  try {
+    console.log("🔵 Démarrage du serveur...");
+    console.log("🔵 PORT =", env.port);
+    console.log("🔵 NODE_ENV =", env.nodeEnv);
 
-  const serveur = app.listen(env.port, "0.0.0.0", () => {
-    console.log(
-      `🚀 API démarrée sur http://localhost:${env.port} (${env.nodeEnv})`,
-    );
-  });
+    console.log("🟡 Connexion à MongoDB...");
+    await connectDB();
+    console.log("🟢 MongoDB connectée !");
 
-  process.on("unhandledRejection", (err) => {
-    console.error("💥 Rejet de promesse non géré :", err);
-    serveur.close(() => process.exit(1));
-  });
+    const serveur = app.listen(env.port, "0.0.0.0", () => {
+      console.log(`🚀 API démarrée sur le port ${env.port} (${env.nodeEnv})`);
+    });
+
+    process.on("unhandledRejection", (err) => {
+      console.error("💥 Rejet de promesse non géré :", err);
+      serveur.close(() => process.exit(1));
+    });
+  } catch (error) {
+    console.error("🔴 ERREUR AU DÉMARRAGE :", error);
+    process.exit(1);
+  }
 }
 
 demarrer();
